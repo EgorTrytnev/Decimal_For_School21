8e13ef18d3d26fa339569ffededdc1b9808b0293
EgorTrytnev <tryegor41@gmail.com>
2025-09-03 20:04:12 +0700

    Start point

---
+++ src/s21_decimal_comparison.cpp
@@ -0,0 +1,28 @@
+#include "s21_decimal_comparison.h"
+
+
+
+int s21_is_less(s21_decimal, s21_decimal)
+{
+    return 0;
+}
+int s21_is_less_or_equal(s21_decimal, s21_decimal)
+{
+    return 0;
+}
+int s21_is_greater(s21_decimal, s21_decimal)
+{
+    return 0;
+}
+int s21_is_greater_or_equal(s21_decimal, s21_decimal)
+{
+    return 0;
+}
+int s21_is_equal(s21_decimal, s21_decimal)
+{
+    return 0;
+}
+int s21_is_not_equal(s21_decimal, s21_decimal)
+{
+    return 0;
+}
